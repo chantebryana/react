f36fb671d2cfe468cf8d2df922cba359e72c5e7a
chantebryana <chantebryana@gmail.com>
2020-07-14 16:24:15 -0500

    finish section 2 of udemy

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 //create a react component 
 const App = function () {
+    const buttonText = 'Click Me!';
+
     return (
         <div>
-            <label class="label" for="name">Enter name:</label>
+            <label className="label" for="name">Enter name:</label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white'}}>Submit</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
         </div>
     );
 };
